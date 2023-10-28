dc562d0d5c69457b096c4a762048e428fb5b31cf
sanchesanri <89216270+sanchesanri@users.noreply.github.com>
2023-10-28 16:19:09 +0000

    Deploying to gh-pages from @ sanchesanri/goit-js-hw-09@086df52f928360da97f5b87bed0b9dc990bf73e7 🚀

---
+++ 01-color-switcher.dc66cdfe.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){if(n)return;a=1e3,n=id=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),a),t.disabled=!0,e.disabled=!1;var a})),e.addEventListener("click",(function(){clearInterval(n),n=null,e.disabled=!0,t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.dc66cdfe.js.map
